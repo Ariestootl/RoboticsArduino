b43b06bc36834271c0b13c998dba4a834477a649
Jose Aries E. De Los Santos <Ariestotle@users.noreply.github.com>
2018-04-19 12:02:13 +0800

    Add files via upload

---
+++ Sensor.ino
@@ -0,0 +1,41 @@
+#define echoPin 7
+#define trigPin 8
+#define LEDPin 13
+
+int maximumRange = 200;
+int minimumRange = 0;
+long duration, distance;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(LEDPin, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  //Serial.println(duration);
+  
+  //Speed of Sound
+  distance = duration/59;
+  
+  if (distance >= maximumRange || distance <= minimumRange) {
+    Serial.println("-1");
+    digitalWrite(LEDPin, HIGH);
+  }
+  else {
+    Serial.println(distance);
+    digitalWrite(LEDPin, LOW);
+  }
+  
+  delay(50);
+}
+  
